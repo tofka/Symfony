7a4252b361e00555e817bebbbb7c0a31ee1eddc4
Karin <karintoft@hotmail.com>
2013-06-12 17:00:57 +0200

    Joina tabeller

--- src/Blogger/BlogBundle/Entity/Repository/BlogRepository.php
+++ src/Blogger/BlogBundle/Entity/Repository/BlogRepository.php
@@ -12,16 +12,19 @@ use Doctrine\ORM\EntityRepository;
  */
 class BlogRepository extends EntityRepository
 {
-    public function getLatestBlogs($limit = null)
-    {
-        $qb = $this->createQueryBuilder('b')
-                   ->select('b')
-                   ->addOrderBy('b.created', 'DESC');
+    
 
-        if (false === is_null($limit))
-            $qb->setMaxResults($limit);
+  public function getLatestBlogs($limit = null)
+  {
+      $qb = $this->createQueryBuilder('b')
+                 ->select('b, c')
+                 ->leftJoin('b.comments', 'c')
+                 ->addOrderBy('b.created', 'DESC');
 
-        return $qb->getQuery()
-                  ->getResult();
-    }
+      if (false === is_null($limit))
+          $qb->setMaxResults($limit);
+
+      return $qb->getQuery()
+                ->getResult();
+  }
 }
